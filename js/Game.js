0281c77d54d45a4eb792e87190e78dfab954dcd5
Harshil Kakkar <kakkar.harshil@gmail.com>
2021-02-13 20:52:22 +1100

    test

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,45 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref("playerCount").once("value");     
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
     }
   }
-}
+  
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 150,200)
+    Player.getPlayerInfo();
+    if(allPlayers !== undefined){
+      var displayPosition = 130;
+      displayPosition += 20;
+      for(var plr in allPlayers){
+        if(plr === "player"+ player.index){
+          fill("red")
+        }
+        else{
+         fill("black")
+        }
+      textSize(15);
+      text(allPlayers[plr].name +":"+ allPlayers[plr].distance, 120, displayPosition);
+      }
+    } 
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+   
+    player.distance += 50;
+    player.update();
+    }
+  }
+
+     
+ }
+
--- js/Player.js
+++ js/Player.js
@@ -1,5 +1,11 @@
 class Player {
-  constructor(){}
+  constructor(){
+   this.name = null;
+   this.index = null;
+   this.distance = 0; 
+
+
+  }
 
   getCount(){
     var playerCountRef = database.ref('playerCount');
@@ -15,9 +21,17 @@ class Player {
   }
 
   update(name){
-    var playerIndex = "player" + playerCount;
+    var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
-      name:name
+      name:this.name,
+      distance:this.distance
     });
+  }
+
+  static getPlayerInfo(){
+  var playerInfo = database.ref('players');
+  playerInfo.on("value", (data)=>{
+    allPlayers = data.val();
+    }) 
   }
 }
